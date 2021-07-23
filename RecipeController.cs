844f03e9e1a656b8a4167f3f8183b0946beea637
Isibeal <isigirluk@yahoo.co.uk>
2021-07-23 07:14:54 +0100

    Add files via upload

---
+++ RecipeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mynewweb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        //next add an action method
+        [HttpGet]
+
+        public string[] GetDishes()
+        {
+            String[] dishes = { "mango", "rice hot", "dumpling" };
+            return dishes;
+        }
+    }
+}
